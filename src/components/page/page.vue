dcfe34f9bae5bc4b5a78e5c80e269f29db74d1f2
yunser <>
2020-05-28 16:23:22 +0800

    update

--- src/components/page/page.vue
+++ src/components/page/page.vue
@@ -1,14 +1,18 @@
 <template>
-    <ui-page :title="title || page.title" :page="page" :backable="backable" ref="page">
+    <ui-page :title="title || page.title" :page="page" ref="page" :backable="backable">
         <div slot="drawer">
-            <ui-appbar title=""></ui-appbar>
+            <div class="header">
+                <img class="logo" src="https://icons.yunser.com/icons/tool.svg">
+            </div>
             <ui-list @itemClick="toggle()">
                 <ui-list-item title="首页" to="/">
-                    <ui-icon slot="left" value="grade"/>
+                    <ui-icon slot="left" value="home"/>
                 </ui-list-item>
-                <ui-list-item title="关于" to="/about">
-                    <ui-icon slot="left" value="grade"/>
+                <ui-list-item title="关于" href="https://project.yunser.com/products/6fe08dd0a0ba11eaa3824f27432dbf16" target="_blank">
+                    <ui-icon slot="left" value="info"/>
                 </ui-list-item>
+            </ui-list>
+            <ui-list @itemClick="toggle()">
             </ui-list>
         </div>
         <slot></slot>
@@ -54,30 +58,16 @@
 </script>
 
 <style lang="scss" scoped>
-    .ui-page {
-        position: absolute;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-    }
-    .page-body {
-        min-height: 500px;
-    }
-    .container-main {
-        padding-top: 16px;
-    }
-    .page-content {
-        position: absolute;
-        top: 0;
-        left: 256px;
-        right: 0;
-        bottom: 0;
-    }
-    .page-container {
-        padding: 16px;
-    }
-    .admin-container {
-        padding: 16px;
-    }
+.ic {
+    color: #009688;
+}
+.header {
+    padding: 40px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+}
+.logo {
+    display: block;
+    width: 80px;
+    margin: 0 auto;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
-    <my-page title="请升级浏览器">
-        <div class="tip">你的浏览器版本太旧，影响了网站的正常使用。建议升级浏览器，以获得更好的体验。</div>
-        <a class="download" href="http://rj.baidu.com/soft/detail/14744.html" target="_blank">下载谷歌浏览器</a>
+    <my-page title="升级浏览器">
+        <!-- <div class="tip"></div> -->
+        <!-- <a class="download" href="http://rj.baidu.com/soft/detail/14744.html" target="_blank">下载谷歌浏览器</a> -->
+        <ui-article>
+            <p>你的浏览器版本太旧，影响了网站的正常使用。建议升级浏览器，以获得更好的体验。</p>
+            <p>建议使用「谷歌浏览器」。</p>
+            <ul>
+                <li>
+                    <a href="https://www.google.cn/chrome/" target="_blank">官方下载地址</a>
+                </li>
+                <li>
+                    <a href="https://pc.qq.com/search.html#!keyword=%E8%B0%B7%E6%AD%8C%E6%B5%8F%E8%A7%88%E5%99%A8" target="_blank">腾讯软件中心</a>
+                </li>
+            </ul>
+        </ui-article>
     </my-page>
 </template>
 
